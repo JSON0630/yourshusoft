305841136b7b3cfa1c278bd284de7907b3558f4a
niudazhuang <dengwm666@icloud.com>
2020-06-07 14:08:17 +0800

    feat http

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App'
+import http from '@/api'
 
 Vue.config.productionTip = false
 App.mpType = 'app'
+Vue.protoType.$http = http
 
 const app = new Vue(App)
 app.$mount()
